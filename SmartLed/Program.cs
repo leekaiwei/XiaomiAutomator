502bbe847aa3882f0812ca6f12ba1fc67d06ca36
Kevin Lee <kevin.lee@heycoins.com>
2021-02-28 21:18:20 +0000

    Initial working version

---
+++ SmartLed/Program.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+using Miio.Devices.Exceptions;
+using Miio.Devices.Implementations.Yeelight;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SmartLed
+{
+    public class Program
+    {
+        private static DateTime timeToActivate;
+        private static IList<GenericYeelightDevice> _devices = new List<GenericYeelightDevice>();
+        private static readonly IAsyncPolicy _policy = Policy
+            .Handle<DeviceCommunicationException>()
+            .WaitAndRetryForeverAsync(retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        private static readonly IConfiguration _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+        static async Task Main()
+        {
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(DetectSessionSwitch);
+
+            Console.WriteLine("Getting sunset data...");
+
+            if (_configuration.GetValue<bool>("Testing"))
+            {
+                timeToActivate = DateTime.UtcNow;
+            }
+            else
+            {
+                var http = new HttpClient();
+                var location = _configuration.GetSection("Location");
+                var response = await http.GetAsync("https://api.sunrise-sunset.org/json?lat=" + location.GetValue<string>("Latitude") + "&lng=" + location.GetValue<string>("Longitude"));
+                var data = JsonSerializer.Deserialize<JsonElement>(await response.Content.ReadAsStringAsync());
+                var sunsetProperty = data.GetProperty("results").GetProperty("sunset").ToString();
+                timeToActivate = DateTime.ParseExact(sunsetProperty, "h:mm:ss tt", null).AddMinutes(-30);
+            }
+
+            Console.WriteLine("Active Time: {0}", timeToActivate);
+
+            foreach (var device in _configuration.GetSection("Devices").GetChildren())
+            {
+                var yeelightDevice = new GenericYeelightDevice(device["Ip"], device["Token"], device["Name"]);
+
+                bool handshaked;
+                do
+                {
+                    Console.WriteLine("Initialising...");
+                    handshaked = await yeelightDevice.MakeHandshake();
+                }
+                while (!handshaked);
+
+                _devices.Add(yeelightDevice);
+            }
+
+            Console.WriteLine("Initialised!");
+
+            if (DateTime.UtcNow >= timeToActivate)
+            {
+                await TurnOn();
+            }
+            else
+            {
+                var timeToActive = DateTime.UtcNow - timeToActivate;
+                await Task.Delay(timeToActive);
+
+                await TurnOn();
+            }
+
+            Console.ReadLine();
+
+            SystemEvents.SessionSwitch -= DetectSessionSwitch;
+        }
+
+        private static async void DetectSessionSwitch(object sender, SessionSwitchEventArgs eventArguments)
+        {
+            if (eventArguments.Reason == SessionSwitchReason.SessionLock)
+            {
+                await TurnOff();
+            }
+            else if (eventArguments.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                await TurnOn();
+            }
+        }
+
+        private static async Task TurnOn()
+        {
+            Console.WriteLine("Turning on...");
+            
+            foreach (var device in _devices)
+            {
+                await _policy.ExecuteAsync(() => device.TurnOnSmoothly(1000));
+            }
+
+            Console.WriteLine("Turned on!");
+        }
+
+        private static async Task TurnOff()
+        {
+            Console.WriteLine("Turning off...");
+
+            if (!_configuration.GetValue<bool>("Testing"))
+            {
+                await Task.Delay(_configuration.GetValue<int>("Delay"));
+            }
+
+            foreach (var device in _devices)
+            {
+                await _policy.ExecuteAsync(() => device.TurnOffSmoothly(1000));
+            }
+
+            Console.WriteLine("Turned off!");
+        }
+    }
+}
